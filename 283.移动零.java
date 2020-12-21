fedb43cb4bdc25b1ea4e6d7678b7e065af962810
sword <1163201968@qq.com>
2020-12-21 08:57:39 +0800

    移动零

--- 283.移动零.java
+++ 283.移动零.java
@@ -9,89 +9,40 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     public void moveZeroes(int[] nums) {
-        moveZeroes5(nums);
+        moveZeroes2(nums);
     }
-
+   
     public void moveZeroes1(int[] nums) {
+       int[] nums1 = new int[nums.length];
+       int count = 0;
+       for(int i=0; i<nums.length; i++) {
+           if(nums[i] != 0) {
+               nums1[count++] = nums[i];
+           }
+       }
 
-        int nums1[] = new int[nums.length];
-        int index = 0;
-        for (int i=0; i<nums.length; i++) {
-            if(nums[i] != 0) {
-                nums1[index++] = nums[i];
-            }
-        }
-
-        for(int i = index; i<nums.length; i++) {
-            nums1[i] = 0;
-        }
-
-        for(int i=0; i<nums.length; i++){
-            nums[i] = nums1[i];
-        }
+       for(int i=0; i<nums.length; i++){
+           if(i < count) {
+               nums[i] = nums1[i];
+           } else {
+               nums[i] = 0;
+           }
+       }
     }
 
     public void moveZeroes2(int[] nums) {
-        int nonZeroTail = 0;
-        for (int i=0; i<nums.length; i++) {
-            if(nums[i] != 0) {
-                //此处没有过滤掉 nonZeroTail 和 i 相等的情况，会出现多余的拷贝
-                nums[nonZeroTail++] = nums[i];
-            }
-        }
+        int lastZeroIndex = 0;
 
-        for(int i=nonZeroTail; i<nums.length; i++) {
-            nums[i] = 0;
-        }
-    }
-
-    public void moveZeroes3(int[] nums) {
-        int nonZeroTail = 0;
         for(int i=0; i<nums.length; i++) {
-            if (nums[i] != 0) {
-                if(i != nonZeroTail) {
-                    nums[nonZeroTail] = nums[i];
+            if(nums[i] != 0){
+                if(i != lastZeroIndex) {
+                    nums[lastZeroIndex] = nums[i];
                     nums[i] = 0;
                 }
-                nonZeroTail++;
+                lastZeroIndex ++;
             }
         }
     }
-
-    
-   //四刷
-   public void moveZeroes4(int[] nums) {
-       int[] num1 = new int[nums.length];
-       int index = 0;
-
-       for(int i: nums) {
-           if(i != 0) {
-                num1[index++] = i;
-           }
-       }
-
-       for(int i = index; i<num1.length; i++){
-           num1[i] = 0;
-       }
-
-       index = 0;
-       for(int i: num1){
-            nums[index++] = i;
-       }
-   }
-
-   public void moveZeroes5(int[] nums) {
-       int lastNonZero = 0;
-       for(int i=0; i<nums.length; i++){
-           if(nums[i] != 0){
-               if(i != lastNonZero){
-                   nums[lastNonZero] = nums[i];
-                   nums[i] = 0;
-               }
-               lastNonZero ++;
-           }
-       }
-   }
 }
 // @lc code=end
 
